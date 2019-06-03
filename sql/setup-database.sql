a99035641c5838537373d6ef46dee13794d25ffa
Aaron <kyrumx@gmail.com>
2019-06-03 13:53:10 +0200

    Quick fix

--- sql/setup-database.sql
+++ sql/setup-database.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 9.6.1
 -- Dumped by pg_dump version 9.6.1
 
--- Started on 2019-06-03 13:20:26
+-- Started on 2019-06-03 13:50:08
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -16,23 +16,6 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- TOC entry 1 (class 3079 OID 12387)
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- TOC entry 2208 (class 0 OID 0)
--- Dependencies: 1
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
 SET search_path = public, pg_catalog;
 
 SET default_tablespace = '';
@@ -40,7 +23,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 188 (class 1259 OID 71734)
+-- TOC entry 186 (class 1259 OID 72158)
 -- Name: addresses; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -48,6 +31,7 @@ CREATE TABLE addresses (
     id integer NOT NULL,
     address character varying(255) NOT NULL,
     address_num integer NOT NULL,
+    address_annex character varying(1),
     city character varying(255) NOT NULL,
     postalcode character varying(6) NOT NULL,
     country character varying(5) NOT NULL
@@ -57,7 +41,7 @@ CREATE TABLE addresses (
 ALTER TABLE addresses OWNER TO postgres;
 
 --
--- TOC entry 187 (class 1259 OID 71732)
+-- TOC entry 185 (class 1259 OID 72156)
 -- Name: addresses_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -72,8 +56,8 @@ CREATE SEQUENCE addresses_id_seq
 ALTER TABLE addresses_id_seq OWNER TO postgres;
 
 --
--- TOC entry 2209 (class 0 OID 0)
--- Dependencies: 187
+-- TOC entry 2197 (class 0 OID 0)
+-- Dependencies: 185
 -- Name: addresses_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -81,16 +65,16 @@ ALTER SEQUENCE addresses_id_seq OWNED BY addresses.id;
 
 
 --
--- TOC entry 189 (class 1259 OID 71743)
+-- TOC entry 187 (class 1259 OID 72167)
 -- Name: cards; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE cards (
     id uuid NOT NULL,
     card_code character varying(255) NOT NULL,
-    is_activated boolean NOT NULL,
+    is_activated boolean DEFAULT false NOT NULL,
     credits numeric(10,2) NOT NULL,
-    is_blocked boolean NOT NULL,
+    is_blocked boolean DEFAULT false NOT NULL,
     requested_at timestamp with time zone NOT NULL,
     activated_at timestamp with time zone,
     user_id_id uuid NOT NULL
@@ -100,7 +84,7 @@ CREATE TABLE cards (
 ALTER TABLE cards OWNER TO postgres;
 
 --
--- TOC entry 190 (class 1259 OID 71748)
+-- TOC entry 188 (class 1259 OID 72172)
 -- Name: clubs; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -116,7 +100,7 @@ CREATE TABLE clubs (
 ALTER TABLE clubs OWNER TO postgres;
 
 --
--- TOC entry 197 (class 1259 OID 71782)
+-- TOC entry 195 (class 1259 OID 72206)
 -- Name: deposits; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -131,7 +115,7 @@ CREATE TABLE deposits (
 ALTER TABLE deposits OWNER TO postgres;
 
 --
--- TOC entry 196 (class 1259 OID 71780)
+-- TOC entry 194 (class 1259 OID 72204)
 -- Name: deposits_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -146,8 +130,8 @@ CREATE SEQUENCE deposits_id_seq
 ALTER TABLE deposits_id_seq OWNER TO postgres;
 
 --
--- TOC entry 2210 (class 0 OID 0)
--- Dependencies: 196
+-- TOC entry 2198 (class 0 OID 0)
+-- Dependencies: 194
 -- Name: deposits_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -155,46 +139,7 @@ ALTER SEQUENCE deposits_id_seq OWNED BY deposits.id;
 
 
 --
--- TOC entry 186 (class 1259 OID 71723)
--- Name: django_migrations; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE django_migrations (
-    id integer NOT NULL,
-    app character varying(255) NOT NULL,
-    name character varying(255) NOT NULL,
-    applied timestamp with time zone NOT NULL
-);
-
-
-ALTER TABLE django_migrations OWNER TO postgres;
-
---
--- TOC entry 185 (class 1259 OID 71721)
--- Name: django_migrations_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE django_migrations_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE django_migrations_id_seq OWNER TO postgres;
-
---
--- TOC entry 2211 (class 0 OID 0)
--- Dependencies: 185
--- Name: django_migrations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE django_migrations_id_seq OWNED BY django_migrations.id;
-
-
---
--- TOC entry 195 (class 1259 OID 71774)
+-- TOC entry 193 (class 1259 OID 72198)
 -- Name: payments; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -210,7 +155,7 @@ CREATE TABLE payments (
 ALTER TABLE payments OWNER TO postgres;
 
 --
--- TOC entry 194 (class 1259 OID 71772)
+-- TOC entry 192 (class 1259 OID 72196)
 -- Name: payments_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -225,8 +170,8 @@ CREATE SEQUENCE payments_id_seq
 ALTER TABLE payments_id_seq OWNER TO postgres;
 
 --
--- TOC entry 2212 (class 0 OID 0)
--- Dependencies: 194
+-- TOC entry 2199 (class 0 OID 0)
+-- Dependencies: 192
 -- Name: payments_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -234,7 +179,7 @@ ALTER SEQUENCE payments_id_seq OWNED BY payments.id;
 
 
 --
--- TOC entry 193 (class 1259 OID 71763)
+-- TOC entry 191 (class 1259 OID 72187)
 -- Name: penalties; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -251,7 +196,7 @@ CREATE TABLE penalties (
 ALTER TABLE penalties OWNER TO postgres;
 
 --
--- TOC entry 192 (class 1259 OID 71761)
+-- TOC entry 190 (class 1259 OID 72185)
 -- Name: penalties_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -266,8 +211,8 @@ CREATE SEQUENCE penalties_id_seq
 ALTER TABLE penalties_id_seq OWNER TO postgres;
 
 --
--- TOC entry 2213 (class 0 OID 0)
--- Dependencies: 192
+-- TOC entry 2200 (class 0 OID 0)
+-- Dependencies: 190
 -- Name: penalties_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -275,7 +220,7 @@ ALTER SEQUENCE penalties_id_seq OWNED BY penalties.id;
 
 
 --
--- TOC entry 191 (class 1259 OID 71753)
+-- TOC entry 189 (class 1259 OID 72177)
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -288,8 +233,8 @@ CREATE TABLE users (
     last_name character varying(255),
     gender character varying(15),
     date_of_birth date,
-    is_email_verified boolean NOT NULL,
-    role character varying(15) NOT NULL,
+    is_email_verified boolean DEFAULT false NOT NULL,
+    role character varying(15) DEFAULT 'user' NOT NULL,
     address_id integer
 );
 
@@ -297,7 +242,7 @@ CREATE TABLE users (
 ALTER TABLE users OWNER TO postgres;
 
 --
--- TOC entry 2042 (class 2604 OID 71737)
+-- TOC entry 2034 (class 2604 OID 72161)
 -- Name: addresses id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -305,7 +250,7 @@ ALTER TABLE ONLY addresses ALTER COLUMN id SET DEFAULT nextval('addresses_id_seq
 
 
 --
--- TOC entry 2045 (class 2604 OID 71785)
+-- TOC entry 2037 (class 2604 OID 72209)
 -- Name: deposits id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -313,15 +258,7 @@ ALTER TABLE ONLY deposits ALTER COLUMN id SET DEFAULT nextval('deposits_id_seq':
 
 
 --
--- TOC entry 2041 (class 2604 OID 71726)
--- Name: django_migrations id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY django_migrations ALTER COLUMN id SET DEFAULT nextval('django_migrations_id_seq'::regclass);
-
-
---
--- TOC entry 2044 (class 2604 OID 71777)
+-- TOC entry 2036 (class 2604 OID 72201)
 -- Name: payments id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -329,7 +266,7 @@ ALTER TABLE ONLY payments ALTER COLUMN id SET DEFAULT nextval('payments_id_seq':
 
 
 --
--- TOC entry 2043 (class 2604 OID 71766)
+-- TOC entry 2035 (class 2604 OID 72190)
 -- Name: penalties id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -337,7 +274,7 @@ ALTER TABLE ONLY penalties ALTER COLUMN id SET DEFAULT nextval('penalties_id_seq
 
 
 --
--- TOC entry 2049 (class 2606 OID 71789)
+-- TOC entry 2039 (class 2606 OID 72213)
 -- Name: addresses addresses_address_address_num_city_18b2518a_uniq; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -346,7 +283,7 @@ ALTER TABLE ONLY addresses
 
 
 --
--- TOC entry 2051 (class 2606 OID 71742)
+-- TOC entry 2041 (class 2606 OID 72166)
 -- Name: addresses addresses_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -355,7 +292,7 @@ ALTER TABLE ONLY addresses
 
 
 --
--- TOC entry 2053 (class 2606 OID 71747)
+-- TOC entry 2043 (class 2606 OID 72171)
 -- Name: cards cards_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -364,7 +301,7 @@ ALTER TABLE ONLY cards
 
 
 --
--- TOC entry 2058 (class 2606 OID 71752)
+-- TOC entry 2048 (class 2606 OID 72176)
 -- Name: clubs clubs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -373,7 +310,7 @@ ALTER TABLE ONLY clubs
 
 
 --
--- TOC entry 2075 (class 2606 OID 71787)
+-- TOC entry 2065 (class 2606 OID 72211)
 -- Name: deposits deposits_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -382,16 +319,7 @@ ALTER TABLE ONLY deposits
 
 
 --
--- TOC entry 2047 (class 2606 OID 71731)
--- Name: django_migrations django_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY django_migrations
-    ADD CONSTRAINT django_migrations_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 2072 (class 2606 OID 71779)
+-- TOC entry 2062 (class 2606 OID 72203)
 -- Name: payments payments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -400,7 +328,7 @@ ALTER TABLE ONLY payments
 
 
 --
--- TOC entry 2066 (class 2606 OID 71771)
+-- TOC entry 2056 (class 2606 OID 72195)
 -- Name: penalties penalties_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -409,7 +337,7 @@ ALTER TABLE ONLY penalties
 
 
 --
--- TOC entry 2061 (class 2606 OID 71802)
+-- TOC entry 2051 (class 2606 OID 72226)
 -- Name: users users_first_name_last_name_date_of_birth_0ff19396_uniq; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -418,7 +346,7 @@ ALTER TABLE ONLY users
 
 
 --
--- TOC entry 2063 (class 2606 OID 71760)
+-- TOC entry 2053 (class 2606 OID 72184)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -427,7 +355,7 @@ ALTER TABLE ONLY users
 
 
 --
--- TOC entry 2054 (class 1259 OID 71846)
+-- TOC entry 2044 (class 1259 OID 72270)
 -- Name: cards_user_id_id_9325af68; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -435,7 +363,7 @@ CREATE INDEX cards_user_id_id_9325af68 ON cards USING btree (user_id_id);
 
 
 --
--- TOC entry 2055 (class 1259 OID 71795)
+-- TOC entry 2045 (class 1259 OID 72219)
 -- Name: clubs_club_address_id_5d4314c9; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -443,7 +371,7 @@ CREATE INDEX clubs_club_address_id_5d4314c9 ON clubs USING btree (club_address_i
 
 
 --
--- TOC entry 2056 (class 1259 OID 71840)
+-- TOC entry 2046 (class 1259 OID 72264)
 -- Name: clubs_owner_id_2db8d5b5; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -451,7 +379,7 @@ CREATE INDEX clubs_owner_id_2db8d5b5 ON clubs USING btree (owner_id);
 
 
 --
--- TOC entry 2073 (class 1259 OID 71839)
+-- TOC entry 2063 (class 1259 OID 72263)
 -- Name: deposits_card_id_id_6d01cdd0; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -459,7 +387,7 @@ CREATE INDEX deposits_card_id_id_6d01cdd0 ON deposits USING btree (card_id_id);
 
 
 --
--- TOC entry 2069 (class 1259 OID 71832)
+-- TOC entry 2059 (class 1259 OID 72256)
 -- Name: payments_card_id_id_e9ee5c9d; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -467,7 +395,7 @@ CREATE INDEX payments_card_id_id_e9ee5c9d ON payments USING btree (card_id_id);
 
 
 --
--- TOC entry 2070 (class 1259 OID 71833)
+-- TOC entry 2060 (class 1259 OID 72257)
 -- Name: payments_club_id_6ab8b231; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -475,7 +403,7 @@ CREATE INDEX payments_club_id_6ab8b231 ON payments USING btree (club_id);
 
 
 --
--- TOC entry 2064 (class 1259 OID 71819)
+-- TOC entry 2054 (class 1259 OID 72243)
 -- Name: penalties_handed_out_by_id_11a76981; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -483,7 +411,7 @@ CREATE INDEX penalties_handed_out_by_id_11a76981 ON penalties USING btree (hande
 
 
 --
--- TOC entry 2067 (class 1259 OID 71820)
+-- TOC entry 2057 (class 1259 OID 72244)
 -- Name: penalties_received_at_id_41faf01e; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -491,7 +419,7 @@ CREATE INDEX penalties_received_at_id_41faf01e ON penalties USING btree (receive
 
 
 --
--- TOC entry 2068 (class 1259 OID 71821)
+-- TOC entry 2058 (class 1259 OID 72245)
 -- Name: penalties_recipient_id_id_dbab4a76; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -499,7 +427,7 @@ CREATE INDEX penalties_recipient_id_id_dbab4a76 ON penalties USING btree (recipi
 
 
 --
--- TOC entry 2059 (class 1259 OID 71803)
+-- TOC entry 2049 (class 1259 OID 72227)
 -- Name: users_address_id_96e92564; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -507,7 +435,7 @@ CREATE INDEX users_address_id_96e92564 ON users USING btree (address_id);
 
 
 --
--- TOC entry 2076 (class 2606 OID 71847)
+-- TOC entry 2066 (class 2606 OID 72271)
 -- Name: cards cards_user_id_id_9325af68_fk_users_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -516,7 +444,7 @@ ALTER TABLE ONLY cards
 
 
 --
--- TOC entry 2077 (class 2606 OID 71790)
+-- TOC entry 2067 (class 2606 OID 72214)
 -- Name: clubs clubs_club_address_id_5d4314c9_fk_addresses_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -525,7 +453,7 @@ ALTER TABLE ONLY clubs
 
 
 --
--- TOC entry 2078 (class 2606 OID 71841)
+-- TOC entry 2068 (class 2606 OID 72265)
 -- Name: clubs clubs_owner_id_2db8d5b5_fk_users_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -534,7 +462,7 @@ ALTER TABLE ONLY clubs
 
 
 --
--- TOC entry 2085 (class 2606 OID 71834)
+-- TOC entry 2075 (class 2606 OID 72258)
 -- Name: deposits deposits_card_id_id_6d01cdd0_fk_cards_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -543,7 +471,7 @@ ALTER TABLE ONLY deposits
 
 
 --
--- TOC entry 2083 (class 2606 OID 71822)
+-- TOC entry 2073 (class 2606 OID 72246)
 -- Name: payments payments_card_id_id_e9ee5c9d_fk_cards_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -552,7 +480,7 @@ ALTER TABLE ONLY payments
 
 
 --
--- TOC entry 2084 (class 2606 OID 71827)
+-- TOC entry 2074 (class 2606 OID 72251)
 -- Name: payments payments_club_id_6ab8b231_fk_clubs_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -561,7 +489,7 @@ ALTER TABLE ONLY payments
 
 
 --
--- TOC entry 2080 (class 2606 OID 71804)
+-- TOC entry 2070 (class 2606 OID 72228)
 -- Name: penalties penalties_handed_out_by_id_11a76981_fk_users_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -570,7 +498,7 @@ ALTER TABLE ONLY penalties
 
 
 --
--- TOC entry 2081 (class 2606 OID 71809)
+-- TOC entry 2071 (class 2606 OID 72233)
 -- Name: penalties penalties_received_at_id_41faf01e_fk_clubs_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -579,7 +507,7 @@ ALTER TABLE ONLY penalties
 
 
 --
--- TOC entry 2082 (class 2606 OID 71814)
+-- TOC entry 2072 (class 2606 OID 72238)
 -- Name: penalties penalties_recipient_id_id_dbab4a76_fk_users_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -588,7 +516,7 @@ ALTER TABLE ONLY penalties
 
 
 --
--- TOC entry 2079 (class 2606 OID 71796)
+-- TOC entry 2069 (class 2606 OID 72220)
 -- Name: users users_address_id_96e92564_fk_addresses_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -596,7 +524,7 @@ ALTER TABLE ONLY users
     ADD CONSTRAINT users_address_id_96e92564_fk_addresses_id FOREIGN KEY (address_id) REFERENCES addresses(id) DEFERRABLE INITIALLY DEFERRED;
 
 
--- Completed on 2019-06-03 13:20:26
+-- Completed on 2019-06-03 13:50:08
 
 --
 -- PostgreSQL database dump complete
